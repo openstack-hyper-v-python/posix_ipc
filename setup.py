88e22ff1fcdc3bc1e9c1de7d19d35ce4b5a53628
gabloe <t-gabloe@microsoft.com>
2014-06-03 10:23:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ license = "http://creativecommons.org/licenses/BSD/"
 keywords = "ipc inter-process communication semaphore shared memory shm message queue"
 
 libraries = ['mman','pthread']
+include_dirs = ['C:\MinGW\include']
+
 
 d = prober.probe()
 
@@ -45,6 +47,7 @@ if "REALTIME_LIB_IS_NEEDED" in d:
 ext_modules = [ duc.Extension("posix_ipc", 
                               source_files,
                               libraries=libraries,
+                              include_dirs=include_dirs,
                               depends = [ "posix_ipc_module.c", 
                                           "probe_results.h", 
                                         ],
